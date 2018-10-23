9845772da12d7e3789328c065194c5a8a5eb1bfa
Jonathan Bossenger <jonathanbossenger@gmail.com>
2018-10-23 08:43:25 +0200

    Create Posts table, Controller, web and api routes

---
+++ database/migrations/2018_10_23_061741_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('post_id')->nullable();
+            $table->string('post_title')->nullable();
+            $table->longText('post_content')->nullable();
+            $table->dateTime('post_date')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
